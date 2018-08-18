adab366277415ea7b63006778463e7ea8438f5cb
Shahrukh Ijaz <ijaz@AhmedQasim-Desktop.arbisoft.com>
2018-08-18 15:39:20 +0500

    first commitit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/<user>")
+def index(user = None):
+    return render_template("user.html",user=user)
+
+
+@app.route("/shopping")
+def shopping():
+    foods = ["Chinese", "Tuna", "Beef"]
+    return render_template("shopping.html", foods=foods)
+
+
+if __name__ == '__main__':
+    app.run()
