de63a2d14e9fb4f7e55e896fe2d46527bdb749ec
Shahrukh Ijaz <ijaz@AhmedQasim-Desktop.arbisoft.com>
2018-08-18 16:08:06 +0500

    second  commitit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,22 @@ def index(user = None):
 def shopping():
     foods = ["Chinese", "Tuna", "Beef"]
     return render_template("shopping.html", foods=foods)
+
+@app.route("/login_page")
+def login_page():
+    return render_template("login_page.html")
+
+
+@app.route("/login_verification", methods=['GET', 'POST'])
+def login_verification():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        login_info = dict();
+        login_info = {"username": username, "password": password}
+        return render_template("user_panel.html", login_info=login_info)
+    return render_template("user.html")
 
 
 if __name__ == '__main__':
